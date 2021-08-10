7128bf814b39ab02db7ccd1d979e6b50c727bc13
riska kurnia dewi <riskakurniad@gmail.com>
2021-08-11 01:05:23 +0700

    change html style

--- program/main.go
+++ program/main.go
@@ -20,7 +20,7 @@ func main() {
 }
 
 func sayHi(c echo.Context) error {
-	return c.String(200, "Hi")
+	return c.HTML(200, "<h1 style='color:#8FBC8F'>Hi</h1><p style='color:#2F4F4F'>I'm Riska</p>")
 }
 
 func greeting(c echo.Context) error {
